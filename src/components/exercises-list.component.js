46a857e3df7d5ae39dffd41a0cfc5da237a0384f
Saeed Shareef <saeedhshareef@gmail.com>
2020-08-24 08:44:53 -0400

    Weekend update

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Exercises List component!</p>
+      </div>
+    )
+  }
+}
